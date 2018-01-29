9cde1824f5fd3b370576c4231b49d6b7fa92896f
Christian <christiandres96@gmail.com>
2018-01-29 10:00:32 -0500

    Resumen
    Commit Iteracion 2

---
+++ core/app/autoload.php
@@ -0,0 +1,9 @@
+<?php
+// autoload.php
+// Esta funcion elimina el hecho de estar agregando los modelos manualmente
+spl_autoload_register(function($modelname){
+	if(Model::exists($modelname)){
+		include Model::getFullPath($modelname);
+	}
+});
+?>
